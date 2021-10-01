be9d8e769b3e1be8aa0567a69be0fd14c1fae0c5
andommar <andommartinez@gmail.com>
2021-10-02 01:18:07 +0200

    Updated sql file with inserts and added DbConn class

---
+++ models/DbConn.php
@@ -0,0 +1,52 @@
+<?php
+class DbConn{
+    private $user = "admin";
+    private $pass = "123";
+    private $servername = "localhost";
+    private $dbname = "socialdb";
+    private $charset = "utf8";
+    public $result;
+    public $dbConn;
+    
+    public function __construct() 
+    {
+        $user = $this->user;
+        $pass = $this->pass;
+
+        // $dsn = "mysql:host=".$this->servername."; dbname=".$this->dbname.";charset=".$this->charset.";
+        $dsn = "mysql:host={$this->servername}; dbname={$this->dbname};charset={$this->charset}";
+        
+        try{
+            $this->dbConn = new PDO ($dsn, $this->user, $this->pass);
+        } catch (PDOException $e){
+            echo "Error trying to stablish connection with database" . $e->getMessage() . "<br/>";
+            die();
+        }
+    }
+
+    public function __destruct()
+    {
+        $this->dbConn = null;
+    }
+    // Execute single query statement. Returns next row as an array indexed by column name
+    public function selectquery($sql)
+    {
+        $stmt = null;
+        $this->result = false;
+        if(isset($sql) && $sql != "" && isset($this->dbConn))
+        {
+            $stmt = $this->dbConn->prepare($sql);
+            if($stmt)
+            {
+                $stmt->execute();
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            }
+
+        }
+        return($result);
+    }
+
+
+}
+
+?>
