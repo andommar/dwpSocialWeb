8031556bb833dcd61919d9b6e76822cb3904f253
andommar <andommartinez@gmail.com>
2021-11-02 20:41:28 +0100

    gitignore fix

--- db/constants.php
+++ db/constants.php
@@ -1,5 +1,5 @@
 <?php
 define("DB_SERVER", "localhost");
-define("DB_USER", "Anna");
-define("DB_PASS", "4nn4");
+define("DB_USER", "admin");
+define("DB_PASS", "123");
 define("DB_NAME", "socialdb");
