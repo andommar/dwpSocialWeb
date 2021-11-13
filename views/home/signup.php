3e96841396c23e1de7daa1079f8ffb01c28ec674
Anna Lopez <annalopezribo@gmail.com>
2021-11-13 19:27:39 +0100

    favicon and remote db conflicts

--- views/home/signup.php
+++ views/home/signup.php
@@ -25,7 +25,7 @@ function validate_data($data)
 function generate_rnd_avatar()
 {
   $rnd_number = rand(1, 8);
-  $avatar = 'avatar_' . $rnd_number;
+  $avatar = 'avatar_' . $rnd_number . '.png';
   return $avatar;
 }
 
@@ -39,6 +39,8 @@ function generate_rnd_avatar()
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <title>Sign Up</title>
+  <!-- Web icon -->
+  <link rel="icon" href="../web/img/assets/favicon.ico" type="image/x-icon">
   <!-- Bootstrap 5.1.3 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
